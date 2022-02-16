2305f52866327a53d560cdd8a19571f5b8d8c4f3
dkw99 <dkw0405@outlook.com>
2022-02-16 23:26:32 +1100

    Fixed secret

---
+++ hacks/bootstrap-cluster.sh
@@ -0,0 +1,12 @@
+flux bootstrap github \
+ --owner=$GITHUB_USER \
+ --repository=$GIT_REPO \
+ --branch=main \
+ --path=./cluster/base/flux-system
+ --personal
+
+ flux bootstrap github \
+--owner=$GITHUB_USER \
+--repository=$GITHUB_REPO \
+--branch=main \
+--path=./cluster/base
