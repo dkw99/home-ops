9e96a26d447c7b9302421f6e29374f2bdd270292
dkw99 <dkw04058@gmail.com>
2022-03-22 20:16:56 +1100

    cleaned up hack scripts

--- hack/replace-vip.sh
+++
@@ -1,2 +0,0 @@
-#!/usr/bin/env bash
-sed -i -e 's/192.168.16.250/192.168.16.100/g' provision/kubeconfig
--- hack/update-cluster.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-set -o errexit
-set -o pipefail
-export PROJECT_DIR=$(git rev-parse --show-toplevel)
-cd $PROJECT_DIR
-
-flux --kubeconfig=./provision/kubeconfig reconcile source git flux-system
