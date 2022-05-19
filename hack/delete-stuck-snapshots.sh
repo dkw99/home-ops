b7862d57e08fdd95aa45e96c7530388b825c14af
dkw99 <dkw04058@gmail.com>
2022-05-19 17:15:29 +1000

    hacks

--- hack/delete-stuck-snapshots.sh
+++ hack/delete-stuck-snapshots.sh
@@ -1,15 +1,17 @@
 #!/usr/bin/env bash
+export PROJECT_DIR=$(git rev-parse --show-toplevel)
+KUBECONFIG=${PROJECT_DIR}/provision/kubeconfig
 
-volumesnapshotcontents=$(kubectl get --no-headers volumesnapshotcontents | awk '{print $1}')
+volumesnapshotcontents=$(kubectl --kubeconfig $KUBECONFIG get --no-headers volumesnapshotcontents | awk '{print $1}')
 for volumesnapshotcontent in $volumesnapshotcontents
 do
     kubectl patch volumesnapshotcontents "${volumesnapshotcontent}" -p '{"metadata":{"finalizers":null}}' --type=merge
 done
 
-volumesnapshots=$(kubectl get --no-headers volumesnapshots -A | awk '{print $1","$2}')
+volumesnapshots=$(kubectl --kubeconfig $KUBECONFIG get --no-headers volumesnapshots -A | awk '{print $1","$2}')
 for item in $volumesnapshots
 do
     namespace="$(echo "${item}" | awk -F',' '{print $1}')"
     volumesnapshot="$(echo "${item}" | awk -F',' '{print $2}')"
-    kubectl patch volumesnapshots "${volumesnapshot}" -n "${namespace}" -p '{"metadata":{"finalizers":null}}' --type=merge
+    kubectl --kubeconfig $KUBECONFIG patch volumesnapshots "${volumesnapshot}" -n "${namespace}" -p '{"metadata":{"finalizers":null}}' --type=merge
 done
